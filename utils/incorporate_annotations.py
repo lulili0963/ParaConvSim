6954a2f2d81f933df275bb45d39180ffdd9f3969
Ivan-Sekulic <ivan.sekulic@telepathy.ai>
2024-10-16 21:43:53 +0200

    formating, readability, renaming

--- utils/incorporate_annotations.py
+++ utils/incorporate_annotations.py
@@ -10,11 +10,15 @@ if __name__ == "__main__":
 
     annotation_dict = {}
 
-    with open("data/cast/year_4/2022_evaluation_topics_flattened_duplicated_v1.0.json") as f:
+    with open(
+        "data/cast/year_4/2022_evaluation_topics_flattened_duplicated_v1.0.json"  # noqa
+    ) as f:
         topics = json.load(f)
 
-    with open("data/cast/year_4/Year 4 Topic Annotations - User Query Annotations.csv") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+    with open(
+        "data/cast/year_4/Year 4 Topic Annotations - User Query Annotations.csv"  # noqa
+    ) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
         for line_number, row in enumerate(csv_reader):
             if line_number == 0:
                 continue
@@ -25,15 +29,14 @@ if __name__ == "__main__":
 
                 annotation_dict[turn_id] = {
                     "information_need": information_need,
-                    "utterance_type": utterance_type
+                    "utterance_type": utterance_type,
                 }
 
     for topic in topics:
-        for turn in topic['turn']:
+        for turn in topic["turn"]:
             turn_id = f"{topic['number']}_{turn['number']}"
-            turn["information_need"] = annotation_dict[turn_id]["information_need"]
+            turn["information_need"] = annotation_dict[turn_id]["information_need"]  # noqa
             turn["utterance_type"] = annotation_dict[turn_id]["utterance_type"]
-
 
     with open("data/cast/year_4/annotated_topics.json", "w") as annotated_f:
         json.dump(topics, annotated_f, indent=4, ensure_ascii=True)
