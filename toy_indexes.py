fa70fa948712aa70531eb444defd7d413292777f
lulili0963 <95346078+lulili0963@users.noreply.github.com>
2024-10-17 22:56:16 +0200

    Create toy_indexes.py

---
+++ toy_indexes.py
@@ -0,0 +1,3 @@
+# move the downloaded folder into data/cast/year_4
+import gdown
+gdown.download_folder('https://drive.google.com/drive/folders/17mhkb-s8Qh2QLFBnneU6gHW48pzE3Ydt?usp=sharing', quiet=True)
